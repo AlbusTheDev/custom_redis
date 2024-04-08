5a90b9f639a90d16d50cda85a5ebea040a831756
AlbusTheDev <qusaih42@gmail.com>
2024-04-08 14:42:37 +0400

    5.9

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 const net = require("net");
-const queryMan = require("./lib/queryMan")
+const {queryMan} = require("./lib/queryMan")
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -7,7 +7,7 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => {
   // Handle connection
   connection.on('data', (data) => {
-    queryMan(connection, data);
+    queryMan(connection, data.toString());
   });
 });
 
