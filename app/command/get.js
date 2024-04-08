4e9e86a6580c968c7bfc6c7b9f9ae1faaf878f46
AlbusTheDev <qusaih42@gmail.com>
2024-04-08 15:26:33 +0400

    7.1

--- app/command/get.js
+++ app/command/get.js
@@ -1,5 +1,9 @@
-const get = (connection, obj, item) => {
-    connection.write(`+${obj[item]}\r\n`);
+const get = (connection, obj, key) => {
+    const item = obj[key];
+    if (item.start == 0 || (Date.now() - item.start) < item.px)
+        connection.write(`+${item.msg}\r\n`);
+    else 
+        connection.write("$-1\r\n");
 }
 
 module.exports = {get};
--- app/command/set.js
+++ app/command/set.js
@@ -1,6 +1,8 @@
 const set = (connection, obj, query) => {
     connection.write("+OK\r\n");
-    obj[query[1]] = query[3];
+    if (query[5] && query[5] == "px") {
+        obj[query[1]] = {msg: query[3], start: Date.now(), px:Number(query[7])};
+    } else obj[query[1]] = {msg: query[3], start:0, px:0};
 };
 
 module.exports = {set};
