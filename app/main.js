6e445381344154d6009089d74c399e4492469c24
AlbusTheDev <qusaih42@gmail.com>
2024-04-08 13:53:48 +0400

    5.3

--- app/main.js
+++ app/main.js
@@ -3,10 +3,10 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-const server = net.createServer((connection) => {
+const server = net.createServer((connection, messages) => {
   // Handle connection
   connection.on('data', (data) => {
-    connection.write(data.join(','));
+    connection.write(messages.join('\r\n'));
   });
 });
 
