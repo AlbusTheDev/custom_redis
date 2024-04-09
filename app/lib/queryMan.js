3340be29fb7c2fb84a2d0c4e272477ee26d2ed57
AlbusTheDev <qusaih42@gmail.com>
2024-04-10 02:20:07 +0400

    17.4

--- app/lib/queryMan.js
+++ app/lib/queryMan.js
@@ -27,7 +27,7 @@ const queryMan = (connection, data) => {
         case "psync":
             sendMsg(connection, psync());
             const base64 = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog=="
-            connection.write(`$${Buffer.from(base64).length}\r\n${base64}`)
+            connection.write(`$${Buffer.from(base64, "base64").length}\r\n${base64}`)
             break;
     }
 };
